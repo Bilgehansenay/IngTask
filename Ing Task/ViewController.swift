b3a6b618d58d301ff4637dcb050d8c075d9bc3b7
Bilgehan Senay <bilgehan.senay@sebit.com.tr>
2020-11-06 22:25:36 +0300

    Initial Commit

---
+++ Ing Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ing Task
+//
+//  Created by Bilgehan Senay on 6.11.2020.
+//  Copyright © 2020 Bilgehan Senay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
