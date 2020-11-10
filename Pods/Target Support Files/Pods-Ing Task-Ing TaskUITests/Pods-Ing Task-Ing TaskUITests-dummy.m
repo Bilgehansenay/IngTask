e069616483ff37f5e023292cd25a82f1f9ff8ebc
Bilgehan Senay <bilgehannnoe@gmail.com>
2020-11-10 12:49:56 +0300

    added ing task

---
+++ Pods/Target Support Files/Pods-Ing Task-Ing TaskUITests/Pods-Ing Task-Ing TaskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ing_Task_Ing_TaskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Ing_Task_Ing_TaskUITests
+@end
---
+++ Pods/Target Support Files/Pods-Ing TaskTests/Pods-Ing TaskTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ing_TaskTests : NSObject
+@end
+@implementation PodsDummy_Pods_Ing_TaskTests
+@end
